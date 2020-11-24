6ca24e62a948878d9a141d3743c44b6bcef5dc3e
Fabian Schilf <fabian.schilf@active-value.de>
2020-11-24 10:45:24 +0100

    initial commit

---
+++ lib/hotlap.ex
@@ -0,0 +1,18 @@
+defmodule Hotlap do
+  @moduledoc """
+  Documentation for Hotlap.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hotlap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hotlap_test.exs
@@ -0,0 +1,8 @@
+defmodule HotlapTest do
+  use ExUnit.Case
+  doctest Hotlap
+
+  test "greets the world" do
+    assert Hotlap.hello() == :world
+  end
+end
