d19b843caf73f59dab37c4642c77a5a1ae5b0075
Fabian Schilf <fabian.schilf@active-value.de>
2020-11-24 12:07:12 +0100

    created laotime struct; added create method

--- lib/hotlap.ex
+++ lib/hotlap.ex
@@ -1,18 +1,47 @@
-defmodule Hotlap do
+defmodule Hotlap.Laptime do
   @moduledoc """
   Documentation for Hotlap.
   """
+  defstruct minutes: 0, seconds: 0, miliseconds: 0
 
   @doc """
-  Hello world.
+  Creates a %Hotlap.Laptime struct from the following laptime format: minutes:seconds:miliseconds
 
   ## Examples
-
-      iex> Hotlap.hello()
-      :world
-
+      iex> Hotlap.Laptime.create "01:18.855"
+      %Hotlap.Laptime{minutes: 1, seconds: 18, miliseconds: 855}
   """
-  def hello do
-    :world
+  def create(laptime_string) do
+    case Regex.named_captures(~r/(?<minutes>\d+):(?<seconds>\d+)\.(?<miliseconds>\d+)/, laptime_string) do
+      nil -> {:error, "Given laptime is not a valid format"}
+      %{
+        "minutes" => minutes,
+        "seconds" => seconds,
+        "miliseconds" => miliseconds
+        } -> %Hotlap.Laptime{minutes: String.to_integer(minutes), seconds: String.to_integer(seconds), miliseconds: String.to_integer(miliseconds)}
+    end
+  end
+end
+
+
+defimpl String.Chars, for: Hotlap.Laptime do
+
+  @doc """
+  String representation of a laptime
+  """
+  def to_string(laptime) do
+    minutes = laptime.minutes
+    |> Integer.to_string
+    |> String.pad_leading(2, "0")
+
+    seconds = laptime.seconds
+    |> Integer.to_string
+    |> String.pad_leading(2, "0")
+
+    miliseconds = laptime.miliseconds
+    |> Integer.to_string
+    |> String.pad_leading(3, "0")
+
+    "#{minutes}:#{seconds}.#{miliseconds}"
   end
 end
--- test/hotlap_test.exs
+++ test/hotlap_test.exs
@@ -1,8 +1,20 @@
 defmodule HotlapTest do
   use ExUnit.Case
-  doctest Hotlap
+  doctest Hotlap.Laptime
 
-  test "greets the world" do
-    assert Hotlap.hello() == :world
+  test "creates_laptime" do
+    assert Hotlap.Laptime.create("01:18.855") == %Hotlap.Laptime{minutes: 1, seconds: 18, miliseconds: 855}
+    assert Hotlap.Laptime.create("1:18.22") == %Hotlap.Laptime{minutes: 1, seconds: 18, miliseconds: 22}
+    assert Hotlap.Laptime.create("1:06.2") == %Hotlap.Laptime{minutes: 1, seconds: 6, miliseconds: 2}
+    assert Hotlap.Laptime.create("1:02.033") == %Hotlap.Laptime{minutes: 1, seconds: 2, miliseconds: 33}
+    assert Hotlap.Laptime.create("1:44.005") == %Hotlap.Laptime{minutes: 1, seconds: 44, miliseconds: 5}
+  end
+
+  test "string_repr" do
+    assert "#{%Hotlap.Laptime{minutes: 1, seconds: 33, miliseconds: 202}}" == "01:33.202"
+    assert "#{%Hotlap.Laptime{minutes: 1, seconds: 1, miliseconds: 2}}" == "01:01.002"
+    assert "#{%Hotlap.Laptime{minutes: 2, seconds: 2, miliseconds: 880}}" == "02:02.880"
+    assert "#{%Hotlap.Laptime{minutes: 1, seconds: 0, miliseconds: 12}}" == "01:00.012"
+    assert "#{%Hotlap.Laptime{minutes: 1, seconds: 54, miliseconds: 22}}" == "01:54.022"
   end
 end
